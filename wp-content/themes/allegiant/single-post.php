763de6f4b2f79efd43b40ffa137febaed5b6190b
Vinicius de Santana <vinicius.vts@gmail.com>
2020-11-18 13:51:13 -0300

    feat: related empreend

--- wp-content/themes/allegiant/single-post.php
+++ wp-content/themes/allegiant/single-post.php
@@ -28,5 +28,37 @@
 		<div class="clear"></div>
 	</div>
 </div>
+<div id="relacionados">
+	<div class="container">
+		<h2 class="titulos mb-5  text-center">Empreendimentos <span style="color: #bd2130 ">Relacionados :</span></h2>
+		<div class="row">
+			<?php
+			$actualPost = get_the_ID();
+			global $post;
+			$args = array( 'cat' => '2', 'posts_per_page' => '3', 'exclude' => $actualPost);
+			$myposts = get_posts( $args );
+			foreach( $myposts as $post ) : setup_postdata($post); ?>
+				<div class="col-12 col-sm-12 col-md-4">
+					<a href="<?php the_permalink(); ?>" title="Ver empreendimento">
+						<div class="card shadow-sm mb-5 text-white rounded">
+							<img class="card-img" src="<?php the_post_thumbnail_url(); ?>">
+							<div class="card-img-overlay d-flex justify-content-end align-items-start flex-column">
+								<span><?php the_field('cidade'); ?></span>
+								<h3 class="card-title text-white mt-0"><?php the_title(); ?></h3>
+								<?php //the_content(); ?>
+								<p class="bairro mb-1"><?php the_field('bairro'); ?></p>
+								<p class="dados mb-1"><?php the_field('dormitorios'); ?></p>
+								<p class="dados mb-4"><?php the_field('situacao'); ?></p>
+								<div class="text-right">
+									<button class="btn btn-outline-light btn-sm" href="<?php the_permalink(); ?>">Saiba mais</button>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
