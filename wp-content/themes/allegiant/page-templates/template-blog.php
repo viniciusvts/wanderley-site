83f2046da0833367439639ecb904f91da1121de4
Vinicius de Santana <vinicius.vts@gmail.com>
2020-12-29 14:07:24 -0300

    fix: card is clicable now

--- wp-content/themes/allegiant/page-templates/template-blog.php
+++ wp-content/themes/allegiant/page-templates/template-blog.php
@@ -16,14 +16,16 @@
                         $myposts = get_posts( $args );
                         foreach( $myposts as $post ) : setup_postdata($post); ?>
                             <div class="col-12 col-sm-12 col-md-6">
-                                <div class="card shadow-sm mb-5">
-                                    <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>">
-                                    <div class="card-body">
-                                        <h5 class="card-title mt-3 mb-3"><?php the_title(); ?></h5>
-                                        <?php the_excerpt(); ?>
-                                        <a class="btn btn-outline-danger btn-sm mt-4" href="<?php the_permalink(); ?>">Saiba mais</a>
-                                    </div>                                    
-                                </div>
+                                <a class="no-style" href="<?php the_permalink(); ?>">
+                                    <div class="card shadow-sm mb-5">
+                                        <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>">
+                                        <div class="card-body">
+                                            <h5 class="card-title mt-3 mb-3"><?php the_title(); ?></h5>
+                                            <?php the_excerpt(); ?>
+                                            <a class="btn btn-outline-danger btn-sm mt-4" href="<?php the_permalink(); ?>">Saiba mais</a>
+                                        </div>                                    
+                                    </div>
+                                </a>
                             </div>
                         <?php endforeach; ?>
                 </div>
