44d15bbcfe67b64718d0fec7507fd6c47c50980d
Vinicius de Santana <vinicius.vts@gmail.com>
2021-05-26 17:27:59 -0300

    fix: change position of nav buttons

--- wp-content/themes/allegiant/dna-inc/shortcodes/links-imprensa.php
+++ wp-content/themes/allegiant/dna-inc/shortcodes/links-imprensa.php
@@ -67,7 +67,7 @@
     
     .carrossel1 .owl-nav button {
         position: absolute;
-        top: 30%;
+        top: -30%;
         width: 30px;
         color: white!important;
         margin: 0;
@@ -99,7 +99,7 @@
         $("#carousel1").owlCarousel({
             loop:true,
             nav:true,
-            navText:['<svg xmlns="http://www.w3.org/2000/svg" width="28.143" height="49.371" viewBox="0 0 28.143 49.371"> <path id="Caminho_8275" data-name="Caminho 8275" d="M98.151,27.129,119.38,48.358a3.457,3.457,0,0,0,4.89-4.889L105.485,24.685,124.269,5.9a3.457,3.457,0,0,0-4.89-4.889L98.151,22.241a3.457,3.457,0,0,0,0,4.888Z" transform="translate(-97.139 0)" fill="#fff"/></svg>','<svg xmlns="http://www.w3.org/2000/svg" width="28.143" height="49.371" viewBox="0 0 28.143 49.371"><path id="Caminho_8275" data-name="Caminho 8275" d="M124.269,27.129,103.04,48.357a3.457,3.457,0,0,1-4.89-4.889l18.784-18.784L98.151,5.9a3.457,3.457,0,0,1,4.89-4.889L124.27,22.241a3.457,3.457,0,0,1,0,4.888Z" transform="translate(-97.139 0)" fill="#fff"/></svg>'],
+            navText:['<svg xmlns="http://www.w3.org/2000/svg" width="28.143" height="49.371" viewBox="0 0 28.143 49.371"> <path id="Caminho_8275" data-name="Caminho 8275" d="M98.151,27.129,119.38,48.358a3.457,3.457,0,0,0,4.89-4.889L105.485,24.685,124.269,5.9a3.457,3.457,0,0,0-4.89-4.889L98.151,22.241a3.457,3.457,0,0,0,0,4.888Z" transform="translate(-97.139 0)" fill="#013b6b"/></svg>','<svg xmlns="http://www.w3.org/2000/svg" width="28.143" height="49.371" viewBox="0 0 28.143 49.371"><path id="Caminho_8275" data-name="Caminho 8275" d="M124.269,27.129,103.04,48.357a3.457,3.457,0,0,1-4.89-4.889l18.784-18.784L98.151,5.9a3.457,3.457,0,0,1,4.89-4.889L124.27,22.241a3.457,3.457,0,0,1,0,4.888Z" transform="translate(-97.139 0)" fill="#013b6b"/></svg>'],
             margin:15,
             responsiveClass:true,
             responsive:{
